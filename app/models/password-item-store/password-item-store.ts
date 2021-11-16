07244729537312289b512f14642961d1e3915cc7
Lowell List <lowell.list@gmail.com>
2021-11-16 12:21:34 -0800

    add some dummy actions to the PasswordItemStore

--- app/models/password-item-store/password-item-store.ts
+++ app/models/password-item-store/password-item-store.ts
@@ -1,5 +1,5 @@
-import { Instance, SnapshotOut, types } from "mobx-state-tree"
-import { PasswordItemModel } from ".."
+import { flow, Instance, SnapshotOut, types } from "mobx-state-tree"
+import { PasswordItem, PasswordItemModel, PasswordItemSnapshot, withEnvironment } from ".."
 
 /**
  * Model description here for TypeScript hints.
@@ -9,8 +9,29 @@ export const PasswordItemStoreModel = types
   .props({
     passwordItems: types.optional(types.array(PasswordItemModel), []),
   })
+  .extend(withEnvironment)
   .views((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
-  .actions((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
+  .actions((self) => ({
+    savePasswordItems: (passwordItemSnapshots: PasswordItemSnapshot[]) => {
+      const passwordItemModels: PasswordItem[] = passwordItemSnapshots.map((c) =>
+        PasswordItemModel.create(c),
+      ) // create model instances from the plain objects
+      self.passwordItems.replace(passwordItemModels) // Replace the existing data with the new data
+    },
+  }))
+  .actions((self) => ({
+    loadInitialPasswordItems: flow(function* () {
+      const aPasswordItem: PasswordItemSnapshot = {
+        id: "1234",
+        name: "My Password",
+        description: "A very cool PW indeed",
+        password: "",
+        notes: "",
+      }
+      self.savePasswordItems([aPasswordItem])
+      __DEV__ && console.tron.log("saving it!")
+    }),
+  }))
 
 type PasswordItemStoreType = Instance<typeof PasswordItemStoreModel>
 export interface PasswordItemStore extends PasswordItemStoreType {}
